9eaf9c86295b6ea3adbd238acf478a385171c737
Eduardo Guedes Pereira Silva <eduardoguedespsn@gmail.com>
2023-01-05 13:55:43 -0300

    Initial DDD Project

---
+++ 4Ucode.Treinamentos/src/Treinamentos.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Treinamentos.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
