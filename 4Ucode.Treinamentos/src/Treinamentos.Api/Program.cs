b963824cde56495ac38ba51be5d3cc0029e21601
Eduardo Guedes Pereira Silva <eduardoguedespsn@gmail.com>
2023-01-06 14:00:30 -0300

    Create-Identity DbConfig

--- 4Ucode.Treinamentos/src/Treinamentos.Api/Program.cs
+++ 4Ucode.Treinamentos/src/Treinamentos.Api/Program.cs
@@ -1,25 +1,44 @@
+using Banco.ApiCore.Data;
+using Microsoft.EntityFrameworkCore;
+using Treinamentos.Api.Config;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+//Config:
+
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddEnvironmentVariables();
+
+builder.Services.AddIdentityConfig(builder.Configuration);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseMySql("server=mysql-banco-api.mysql.database.azure.com;initial catalog=4UcodeTreinamentosDB;uid=MysqlRoot;pwd=Mudar#123",
+    Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.0-mysql")).UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+});
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+//Build app:
+
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapControllers();
 
 app.Run();
